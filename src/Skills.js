74e0a2bcbe7449bd84cfe113e43afc78f814729e
nihisd-max <nithishdnr3@gmail.com>
2020-12-21 22:23:34 +0530

    v1

---
+++ src/Skills.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Skills =() =>(
+    <section className="resume-section" id="skills">
+                <div className="resume-section-content">
+                    <h2 className="mb-5">Skills</h2>
+                    <div className="subheading mb-3">Programming Languages & Tools</div>
+                    <ul className="list-inline dev-icons">
+                        <li className="list-inline-item"><i className="fab fa-html5"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-css3-alt"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-js-square"></i></li>
+                        <li className="list-inline-item"><i className="fab fa-react"></i></li>
+                       <li className="list-inline-item"><i className="fab fa-npm"></i></li>
+                       <li className="list-inline-item"><i className="fab fa-python"></i></li>
+                       <li className="list-inline-item"><i className="fab fa-cuttlefish"></i></li>
+                       <li className="list-inline-item"><i className="fab fa-git"></i></li>
+                       <li className="list-inline-item"><i className="fab fa-github"></i></li>
+                    </ul>
+                   
+                </div>
+            </section>
+
+);
+
+export default Skills;
